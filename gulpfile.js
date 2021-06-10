66e735c149aaf05c38ac9d842c9d2bb7a2534fb0
Oleksandra Peleno <alexandragubar@gmail.com>
2021-06-10 09:20:17 +0300

    Change img paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
-// ========================= Setings folders =================================
 
-const project_folder = "dist";  //require("path").basename(__dirname)
+const project_folder = "dist";
 const source_folder = "src";
-
-// ===========================================================================
-// =========================== Setings Path ==================================
 
 const path = {
   build: {
@@ -33,9 +29,6 @@ const path = {
   clean: "./" + project_folder + "/",
 };
 
-// ===========================================================================
-// =========================== Gulp plugins ==================================
-
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
 const fileInclude = require("gulp-file-include");
@@ -51,12 +44,9 @@ const imagemin = require("gulp-imagemin");
 const cheerio = require("gulp-cheerio");
 const babel = require("gulp-babel");
 const ghPages = require("gulp-gh-pages");
-const realFavicon = require('gulp-real-favicon');
+//const realFavicon = require('gulp-real-favicon');
 const del = require("del");
 const fs = require('fs');
-
-// ===========================================================================
-// ============================= All Setings =================================
 
 gulp.task("html", function () { // setting html
   return gulp.src(path.src.html)
@@ -154,76 +144,6 @@ gulp.task('clean', function () { // setting clean
   return del(path.clean);
 });
 
-var FAVICON_DATA_FILE = 'favicon.json';
-
-gulp.task('create-favicon', function (done) { // setting create favicons
-  realFavicon.generateFavicon({
-    masterPicture: source_folder + '/img/favicon.png',
-    dest: project_folder + '/img/icons/',
-    iconsPath: 'img/icons/',
-    design: {
-      ios: {
-        pictureAspect: 'backgroundAndMargin',
-        backgroundColor: '#ffffff',
-        margin: '14%',
-        assets: {
-          ios6AndPriorIcons: false,
-          ios7AndLaterIcons: false,
-          precomposedIcons: false,
-          declareOnlyDefaultIcon: true
-        }
-      },
-      desktopBrowser: {
-        design: 'raw'
-      },
-      windows: {
-        pictureAspect: 'whiteSilhouette',
-        backgroundColor: '#2f675c',
-        onConflict: 'override',
-        assets: {
-          windows80Ie10Tile: false,
-          windows10Ie11EdgeTiles: {
-            small: false,
-            medium: true,
-            big: false,
-            rectangle: false
-          }
-        }
-      },
-      androidChrome: {
-        pictureAspect: 'noChange',
-        themeColor: '#ffffff',
-        manifest: {
-          display: 'standalone',
-          orientation: 'notSet',
-          onConflict: 'override',
-          declared: true
-        },
-        assets: {
-          legacyIcon: false,
-          lowResolutionIcons: false
-        }
-      }
-    },
-    settings: {
-      scalingAlgorithm: 'Mitchell',
-      errorOnImageTooSmall: false,
-      readmeFile: false,
-      htmlCodeFile: false,
-      usePathAsIs: false
-    },
-    markupFile: FAVICON_DATA_FILE
-  }, function () {
-    done();
-  });
-});
-
-gulp.task('inject-favicon', function () { // setting favicon inject
-  return gulp.src(project_folder + '/**/*.html')
-    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
-    .pipe(gulp.dest(project_folder));
-});
-
 gulp.task('watch', function () { // setting watch
   gulp.watch([path.watch.html], gulp.parallel('html'));
   gulp.watch([path.watch.css], gulp.parallel('css'));
@@ -231,15 +151,6 @@ gulp.task('watch', function () { // setting watch
   gulp.watch([path.watch.img], gulp.parallel('img'));
 });
 
-// ===========================================================================
-// ========================== "gulp build" ===================================
-
 gulp.task('build', gulp.series('clean', gulp.parallel('html', 'php', 'js', 'img', 'css', 'fonts')));
-gulp.task('favicon', gulp.series('create-favicon', 'inject-favicon'));
-
-// ===========================================================================
-// =============== default function, after command "gulp" ====================
 
 gulp.task('default', gulp.parallel('build', 'watch', 'browser-sync'));
-
-// ===========================================================================
--- src/js/main.js
+++ src/js/main.js
@@ -50,28 +50,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
   });
 })();
 
-//vanilla JS lazy-load
-
-  const images = document.querySelectorAll('img');
-  const options = {
-    rootMargin: '100px',
-  };
-  const onEntry = (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const image = entry.target;
-        const src = image.dataset.lazy;
-
-        image.src = src;
-        image.classList.add('appear');
-
-        observer.unobserve(image);
-      }
-    });
-  };
-  const io = new IntersectionObserver(onEntry, options);
-  images.forEach(image => io.observe(image));
-
 
 //TIMER
 
